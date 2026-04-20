67f710d5c1354ab62b58950c00ab5ed94d53a514
Marija Barišić <marija.barisic15@gmail.com>
2026-04-20 23:00:50 +0200

    kreirane klase Lokal i Dogadaj s mapiranjem stupaca

---
+++ Backend/Models/Dogadaj.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+/// <summary>
+/// Predstavlja entitet događaja koji je povezan s određenim lokalom.
+/// Sadrži informacije o nazivu, opisu i vremenu održavanja (timestamp).
+/// Implementira One-to-Many relaciju prema entitetu Lokal.
+
+namespace PulsGrada.Models
+{
+    [Table ("dogadaji")]
+    public class Dogadaj
+    {
+        public int Id { get; set; }
+
+        [Column("lokal_id")]
+        public int LokalId { get; set; }
+
+        [Column("organizator_id")]
+        public int OrganizatorId { get; set; }
+
+        [Column("kategorija_id")]
+        public int KategorijaId { get; set; }
+
+        [Column("naziv")]
+        public string Naziv { get; set; } = string.Empty;
+
+        [Column("vrijeme_pocetka")]
+        public DateTime VrijemePocetka { get; set; }
+
+        [Column("opis")]
+        public string Opis { get; set; } = string.Empty;
+
+        [Column("url_slike")]
+        public int UrlSlike { get; set; }
+    }
+}
---
+++ Backend/Models/Lokal.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+/// <summary>
+/// Predstavlja osnovni entitet ugostiteljskog objekta.
+/// Sadrži detaljne informacije o lokalu, njegovim značajkama i pravilima.
+/// Mapira se na tablicu 'lokali' u PostgreSQL bazi podataka.
+/// </summary>
+
+namespace PulsGrada.Models
+{
+    [Table("lokali")]
+    public class Lokal
+    {
+        public int Id { get; set; }
+
+        [Column("naziv")]
+        public string Naziv { get; set; } = string.Empty;
+
+        [Column("adresa")]
+        public string Adresa { get; set; } = string.Empty;
+
+        [Column("lokacija")]
+        public string Lokacija { get; set; } = string.Empty; // Geolokacija trenutno string dok ne povežemo s bazom
+        
+        [Column("radno_vrijeme")]
+        public string RadnoVrijeme { get; set; } = string.Empty;
+
+        [Column("opis")]
+        public string Opis { get; set; } = string.Empty;
+
+        [Column("ima_pusenje")]
+        public bool ImaPusenje { get; set; }
+
+        [Column("ima_biljar")]
+        public bool ImaBiljar { get; set; }
+
+        [Column("ima_pikado")]
+        public bool ImaPikado { get; set; }
+
+        [Column("url_cjenik")]
+        public string UrlCjenik { get; set; } = string.Empty;
+
+        [Column("is_premium")]
+        public bool IsPremium { get; set; }
+    }
+}
