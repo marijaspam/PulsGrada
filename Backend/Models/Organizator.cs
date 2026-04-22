a243ee6104566cd7c36acde8fd866ba568c57a03
Marija Barišić <marija.barisic15@gmail.com>
2026-04-22 23:07:09 +0200

    kreirane Organizator s mapiranjem stupaca

---
+++ Backend/Models/Organizator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+/// <summary>
+/// Predstavlja entitet organizatora događanja (npr. udruga, agencija ili tvrtka).
+/// Sadrži podatke o nazivu, kontakt informacijama i povezanim događajima.
+/// Mapira se na tablicu 'organizatori' u PostgreSQL bazi podataka.
+/// </summary>
+namespace PulsGrada.Models
+{
+    [Table("organizatori")]
+    public class Organizator
+    {
+        public int Id {get; set;}
+
+        [Column("naziv")]
+        public string Naziv { get; set; } = string.Empty;
+
+        [Column("opis")]
+        public string Opis{ get; set; } = string.Empty;
+
+        [Column("Kontakt_email")]
+        public string KontaktEmail { get; set; } = string.Empty;
+    }
+}
