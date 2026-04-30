c03dfb6cad262534fd552cd569189fd55b18ffaf
Marija Barišić <marija.barisic15@gmail.com>
2026-04-30 04:05:16 +0200

    popravljanje buggova

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using PulsGrada.Data;
 using PulsGrada.Repositories;
 using PulsGrada.Services;
 
@@ -9,6 +11,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddScoped<ILokalRepository, LokalRepository>();
 builder.Services.AddScoped<IDogadajRepository, DogadajRepository>();
 builder.Services.AddScoped<IFavoritRepository, FavoritRepository>();
@@ -16,6 +23,12 @@ builder.Services.AddScoped<IRecenzijaRepository, RecenzijaRepository>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 builder.Services.AddScoped<IKategorijaRepository, KategorijaRepository>();
 
+builder.Services.AddScoped<ILokalService, LokalService>();
+builder.Services.AddScoped<IDogadajService, DogadajService>();
+builder.Services.AddScoped<IFavoritService, FavoritService>();
+builder.Services.AddScoped<IRecenzijaService, RecenzijaService>();
+builder.Services.AddScoped<IKategorijaService, KategorijaService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
